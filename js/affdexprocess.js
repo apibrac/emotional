0846f006de6a57126695532fe1a91650cd034ae4
apibrac <alexis.pibrac@gmail.com>
2017-03-28 21:50:14 +0200

    Start cleaning

--- js/affdexprocess.js
+++ js/affdexprocess.js
@@ -9,34 +9,30 @@ $(document).ready(function(){
   //Construct a CameraDetector and specify the image width / height and face detector mode.
   detector = new affdex.CameraDetector(divRoot, width, height, faceMode);
 
-  //Enable detection of all Expressions, Emotions and Emojis classifiers.
   detector.detectAllEmotions();
   detector.detectAllExpressions();
-  detector.detectAllEmojis();
-  detector.detectAllAppearance();
 
   //Add a callback to notify when the detector is initialized and ready for runing.
   detector.addEventListener("onInitializeSuccess", function() {
-    log('#logs', "The detector reports initialized");
+    console.log("Detector initialized");
     //Display canvas instead of video feed because we want to draw the feature points on it
-    $("#face_video_canvas").css("display", "block");
-    $("#face_video").css("display", "none");
+    //$("#face_video_canvas").css("display", "block");
+    //$("#face_video").css("display", "none");
   });
 
   //Add a callback to notify when camera access is allowed
   detector.addEventListener("onWebcamConnectSuccess", function() {
-    log('#logs', "Webcam access allowed");
+    console.log("Webcam connect, thanks bro")
   });
 
   //Add a callback to notify when camera access is denied
   detector.addEventListener("onWebcamConnectFailure", function() {
-    log('#logs', "webcam denied");
-    console.log("Webcam access denied");
+    console.log("Webcam access denied :( :(");
   });
 
   //Add a callback to notify when detector is stopped
   detector.addEventListener("onStopSuccess", function() {
-    log('#logs', "The detector reports stopped");
+    console.log("The detector reports stopped");
     $("#results").html("");
   });
 
@@ -48,14 +44,14 @@ $(document).ready(function(){
     log('#results', "Timestamp: " + timestamp.toFixed(2));
     log('#results', "Number of faces found: " + faces.length);
     if (faces.length > 0) {
-      log('#results', "Appearance: " + JSON.stringify(faces[0].appearance));
+      //log('#results', "Appearance: " + JSON.stringify(faces[0].appearance));
       log('#results', "Emotions: " + JSON.stringify(faces[0].emotions, function(key, val) {
         return val.toFixed ? Number(val.toFixed(0)) : val;
       }));
       log('#results', "Expressions: " + JSON.stringify(faces[0].expressions, function(key, val) {
         return val.toFixed ? Number(val.toFixed(0)) : val;
       }));
-      log('#results', "Emoji: " + faces[0].emojis.dominantEmoji);
+      //log('#results', "Emoji: " + faces[0].emojis.dominantEmoji);
       drawFeaturePoints(image, faces[0].featurePoints);
     }
   });
@@ -68,7 +64,7 @@ $(document).ready(function(){
     var vRatio = contxt.canvas.height / img.height;
     var ratio = Math.min(hRatio, vRatio);
 
-    contxt.strokeStyle = "#FFFFFF";
+    contxt.strokeStyle = "#000000";
     for (var id in featurePoints) {
       contxt.beginPath();
       contxt.arc(featurePoints[id].x,
@@ -89,12 +85,10 @@ function onStart() {
     $("#logs").html("");
     detector.start();
   }
-  log('#logs', "Clicked the start button");
 }
 
 //function executes when the Stop button is pushed.
 function onStop() {
-  log('#logs', "Clicked the stop button");
   if (detector && detector.isRunning) {
     detector.removeEventListener();
     detector.stop();
@@ -103,7 +97,6 @@ function onStop() {
 
 //function executes when the Reset button is pushed.
 function onReset() {
-  log('#logs', "Clicked the reset button");
   if (detector && detector.isRunning) {
     detector.reset();
 
