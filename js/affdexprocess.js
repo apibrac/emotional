45a5cb44972e79f21c3ef74ddc8791a1d07495bd
apibrac <alexis.pibrac@gmail.com>
2017-04-02 20:58:46 +0200

    Superpose video

--- js/affdexprocess.js
+++ js/affdexprocess.js
@@ -4,8 +4,8 @@ $(document).ready(function(){
   // SDK Needs to create video and canvas nodes in the DOM in order to function
   // Here we are adding those nodes a predefined div.
   var divRoot = $("#affdex_elements")[0];
-  var width = 640;
-  var height = 480;
+  var width = 320;
+  var height = 240;
   var faceMode = affdex.FaceDetectorMode.LARGE_FACES;
   //Construct a CameraDetector and specify the image width / height and face detector mode.
   detector = new affdex.CameraDetector(divRoot, width, height, faceMode);
@@ -17,7 +17,7 @@ $(document).ready(function(){
   detector.addEventListener("onInitializeSuccess", function() {
     console.log("Detector initialized");
     //Display canvas instead of video feed because we want to draw the feature points on it
-    $("#face_video_canvas").css("display", "none");
+    $("#face_video_canvas").css("display", "block");
     $("#face_video").css("display", "none");
   });
 
@@ -53,7 +53,7 @@ $(document).ready(function(){
     var vRatio = contxt.canvas.height / img.height;
     var ratio = Math.min(hRatio, vRatio);
 
-    contxt.strokeStyle = "#000000";
+    contxt.strokeStyle = "#FFFFFF";
     for (var id in featurePoints) {
       contxt.beginPath();
       contxt.arc(featurePoints[id].x,
